606f7e7ba88c0c38d0dfe79db5f36e7267c15c45
Himanshikhare <117911062+Himanshikhare@users.noreply.github.com>
2025-07-01 16:46:37 +0530

    Delete Backend directory

--- Backend/models/Project.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const ProjectSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  image: String,
-});
-
-module.exports = mongoose.model("Project", ProjectSchema);
